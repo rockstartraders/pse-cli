0b20babce99b7089ad89cb7e33b3d82ce7367019
root <jamespaulespea@gmail.com>
2020-03-15 01:34:40 +0800

    fix time and date

--- pse.py
+++ pse.py
@@ -1,23 +1,33 @@
-#!//usr/bin/python3.6
+#!/usr/bin/python3
+
 
 import requests
 import json
 import os
 import datetime
 import readline
+import arrow
+import time
+import dateutil.parser
+
 
 
 url = 'http://phisix-api4.appspot.com/stocks/'
 
 while True:
+
+    
     print()
-    a = input(' Stock/Ticker Code: ')  # add u for it to work
+    a = input(u' Stock/Ticker Code: ')  # add u for it to work
+
     if a == 'exit':
      break
     elif a == 'clear':
         os.system('clear')
-        continue  # reload the page
+        continue  # reload the page    
     b = '.json'
+
+
 
 
     try:
@@ -25,11 +35,22 @@ while True:
         pse = requests.get(final).json()
       # print(pse)
 
+
+
         print()        #datetime
         pse_asof = pse['as_of']
-        print(' Date as of: ' + pse_asof.replace('T', '   Time:' ))
-
+        d = dateutil.parser.parse(pse_asof)
+        print(' Date:  ' + d.strftime('%m/%d/%Y'))  # goods na ito 
+        oras=(d.strftime('%H:%M'))
+        oras1 = datetime.datetime.strptime(oras,'%H:%M').strftime('%I:%M %p') # goods narin ito 
+        
+        
+        print(' As of: ' + oras1)
+        
         print()
+
+
+        # stock name na ito 
 
         pse_name = pse['stock'][0]['name']
         print(' Company name: ' + pse_name)
@@ -41,10 +62,18 @@ while True:
        #  print(' Currency: ' + json.dumps(pse_currency))
 
         pse_amount = pse['stock'][0]['price']['amount']
-        print(' Amount: ' + json.dumps(pse_amount) + " " + pse_currency)   # dumps will take all kind of format and value
+        print(u' Amount: ' + '\u20B1 ' + json.dumps(pse_amount)) # \u20B1  # dumps will take all kind of format and value
 
         pse_pc = pse['stock'][0]['percent_change']
-        print(' Percent Change: ' + json.dumps(pse_pc) + '%')
+        aa = float(pse_pc)
+
+        if aa < 0 :
+            print(u' Percent Change: ' + '\u001b[31m' + json.dumps(aa) + '\u001b[0m' + '%')
+        elif aa == 0:
+            print(u' Percent Change: ' + json.dumps(aa) + '%')   
+        else:
+             print(u' Percent Change: ' + '\u001b[32m' + json.dumps(aa) + '\u001b[0m' + '%')
+       
 
         pse_vol = pse['stock'][0]['volume']
         print(' Volume: ' + json.dumps(pse_vol))
@@ -52,13 +81,20 @@ while True:
         print()
         print(u' Please Type \u001b[32mexit\u001b[0m to close this program or\u001b[0m Type \u001b[32mclear\u001b[0m to clear the screen.')
 
-
-
-
+       
     except Exception as e:
-
         print('')
         print('')
         print('{0}{1}'.format(u' Error Occurred !!\u001b[31m \n Either not found or Error With Connectivity. \n ' ,e ))  # string format with catch
         print(u'\u001b[37m Please try again ..\u001b[37m\n')
         print()
+
+
+
+
+  
+
+      
+      
+
+
